1d393e080a44aa3fdccca13a46331692a11c339c
yinan <st171243@stud.uni-stuttgart.de>
2023-01-20 01:26:19 +0100

    Update 1.20

--- Train-model-with-nonlinearProgramming/validation.py
+++ Train-model-with-nonlinearProgramming/validation.py
@@ -2,6 +2,7 @@
 # @Time : 2022/12/7 20:56 
 # @Author : Yinan 
 # @File : Validation.py
+import os
 import pickle
 import numpy as np
 import pandas as pd
@@ -50,6 +51,12 @@ def validate(W, data_input):
     """ initialize hidden state & cell state"""
     C = np.random.randn(5, 1)
     X = np.random.randn(5, 1)
+
+    con = [-1 + (1 + sigmoid(np.linalg.norm(np.hstack((W_o, U_o, b_o)), np.inf))) *
+           sigmoid(np.linalg.norm(np.hstack((W_f, U_f, b_f)), np.inf)),
+           -1 + (1 + sigmoid(np.linalg.norm(np.hstack((W_o, U_o, b_o)), np.inf))) *
+           sigmoid(np.linalg.norm(np.hstack((W_i, U_i, b_i)), np.inf)) * np.linalg.norm(U_c, 1)]
+
     for i in range(len(data_input)):
         C = sigmoid(np.dot(W_f, u.iloc[i]) + np.dot(U_f, X) + b_f) * C + \
             sigmoid(np.dot(W_i, u.iloc[i]) + np.dot(U_i, X) + b_i) * \
@@ -57,55 +64,71 @@ def validate(W, data_input):
         X = sigmoid(np.dot(W_o, u.iloc[i]) + np.dot(U_o, X) + b_o) * tanh(C)
         y_pred = np.dot(W_y.T, X) + b_y
         y_val.append(y_pred.squeeze())
-    return y_val
+    return y_val, con
 
 
 """ define FIT as metric """
 def FIT(y_hat, y):
     return 100 * (1 - np.linalg.norm(y_hat - y) / np.linalg.norm(y))
 
+def nrmse(y, y_hat):  # normalization to y
+    return np.sqrt(1 / len(y)) * np.linalg.norm(y - y_hat)
 
 def main():
     """ load trained model """
-    model_file = open(r"models/model_noise_SLSQP_1e-10_wo_con_2023-01-16-00_54_08.pkl", "rb")
-    model = pickle.load(model_file)
-    W = model.x
-
-    """ train or validation """
-    train = True
-    noise = True
-    if train:
-        """ load training set: 4400 samples """
-        if noise:
-            data_input = pd.read_csv("../data/train/train_input_noise.csv", header=None).iloc[:, 1]
-            y_true = pd.read_csv("../data/train/train_output_noise.csv", header=None).iloc[:, 1]
-        else:
-            data_input = pd.read_csv("../data/train/train_input_clean.csv", header=None).iloc[:, 1]
-            y_true = pd.read_csv("../data/train/train_output_clean.csv", header=None).iloc[:, 1]
-    else:
-        """ load validation set: 2250 samples """
-        if noise:
-            data_input = pd.read_csv("../data/val/val_input_noise.csv", header=None).iloc[:, 1]
-            y_true = pd.read_csv("../data/val/val_output_noise.csv", header=None).iloc[:, 1]
-        else:
-            data_input = pd.read_csv("../data/val/val_input_clean.csv", header=None).iloc[:, 1]
-            y_true = pd.read_csv("../data/val/val_output_clean.csv", header=None).iloc[:, 1]
-
-    """ calculate y_val """
-    y_val = validate(W, data_input)
-
-    """ plot """
-    plt.figure()
-    plt.plot([*range(len(data_input) - 100)], y_val[100:], color='b', label='Prediction')
-    plt.plot([*range(len(data_input) - 100)], y_true[100:], color='r', label='Ground Truth')
-
-    # calculate FIT
-    fit_ = FIT(y_val[100:], y_true[100:])
-    plt.legend()
-    plt.title('pH on {} set with FIT: {:.3f}%'.format('train' if train else "validation", fit_))
-    plt.show()
+    models = os.listdir('./models')
+    for model in models:
+        model_path = './models/' + model
+        model_file = open(model_path, "rb")
+        model = pickle.load(model_file)
+        W = model.x
 
 
+        f, ax = plt.subplots(2, 1)
+        i = 0
+        """ train or validation """
+        for train in [True, False]:
+            noise = True
+            if train:
+                """ load training set: 4400 samples """
+                if noise:
+                    data_input = pd.read_csv("../data/train/train_input_noise.csv", header=None).iloc[:, 1]
+                    y_true = pd.read_csv("../data/train/train_output_noise.csv", header=None).iloc[:, 1]
+                else:
+                    data_input = pd.read_csv("../data/train/train_input_clean.csv", header=None).iloc[:, 1]
+                    y_true = pd.read_csv("../data/train/train_output_clean.csv", header=None).iloc[:, 1]
+            else:
+                """ load validation set: 2250 samples """
+                if noise:
+                    data_input = pd.read_csv("../data/val/val_input_noise.csv", header=None).iloc[:, 1]
+                    y_true = pd.read_csv("../data/val/val_output_noise.csv", header=None).iloc[:, 1]
+                else:
+                    data_input = pd.read_csv("../data/val/val_input_clean.csv", header=None).iloc[:, 1]
+                    y_true = pd.read_csv("../data/val/val_output_clean.csv", header=None).iloc[:, 1]
+
+            """ calculate y_val """
+            y_val, cons = validate(W, data_input)
+
+            """ plot """
+            nrmse_score = nrmse(y_val, y_true)
+            f.suptitle('c1:{} c2:{}'.format(cons[0], cons[1]))
+            ax[i].plot(y_val, color='m', label='pred', alpha=0.8)
+            ax[i].plot(y_true, color='c', label='real', linestyle='--', alpha=0.5)
+            ax[i].tick_params(labelsize=5)
+            ax[i].legend(loc='best')
+            ax[i].set_title('NRMSE on {} set: {:.3f}'.format('train' if train else 'val', nrmse_score), fontsize=8)
+            i += 1
+        plt.savefig('./result/python/fig/{}.jpg'.format(np.random.randint(100)), bbox_inches='tight', dpi=500)
+            # plt.figure()
+            # plt.title('constraints: c1:{}, c2:{}'.format(cons[0], cons[1]))
+            # plt.plot([*range(len(data_input) - 100)], y_val[100:], color='b', label='Prediction')
+            # plt.plot([*range(len(data_input) - 100)], y_true[100:], color='r', label='Ground Truth')
+            #
+            # # calculate FIT
+            # fit_ = FIT(y_val[100:], y_true[100:])
+            # plt.legend()
+            # plt.title('pH on {} set with FIT: {:.3f}%'.format('train' if train else "validation", fit_))
+            # plt.show()
 if __name__ == '__main__':
     main()
 
