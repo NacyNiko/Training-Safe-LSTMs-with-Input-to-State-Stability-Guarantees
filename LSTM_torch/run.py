a9abb42f75e84542e7e8e8da90712b4714ad8e85
yinan <st171243@stud.uni-stuttgart.de>
2023-03-08 12:42:22 +0100

    Update 3.8

--- LSTM_torch/run.py
+++ LSTM_torch/run.py
@@ -11,7 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Input state stable LSTM')
-parser.add_argument('--dataset', default='robot_inverse', choices=['robot_forward', 'robot_forward', 'robot_inverse'], help='LSTM dataset')
+parser.add_argument('--dataset', default='robot_forward', choices=['robot_forward', 'robot_forward', 'robot_inverse'], help='LSTM dataset')
 parser.add_argument('--hidden_size', default=5, help='hidden size of LSTM')
 if parser.parse_args().dataset == 'pHdata':
     input_size = output_size = 1
@@ -25,7 +25,7 @@ parser.add_argument('--input_size', default=input_size, help='input size of LSTM
 parser.add_argument('--output_size', default=output_size, help='output size of output layer')
 parser.add_argument('--layers', default=1, help='number of layers of LSTM')
 parser.add_argument('--batch_size', default=64, help='train batch size')
-parser.add_argument('--epochs', default=60, help='maximum train epochs')
+parser.add_argument('--epochs', default=80, help='maximum train epochs')
 parser.add_argument('--tolerance', default=1e-6, help='minimum tolerance of loss')
 parser.add_argument('--tol_stop', default=1e-10, help='minimum tolerance between 2 epochs')
 parser.add_argument('--len_sequence', default=5, help='length of input sequence to LSTM')
