878f5f7d1d7e28debe12054bb29a4f859a0347ac
yinan <st171243@stud.uni-stuttgart.de>
2023-03-21 12:56:58 +0100

    3.21.1

--- LSTM_torch/run.py
+++ LSTM_torch/run.py
@@ -33,7 +33,7 @@ parser.add_argument('--len_sequence', default=5, help='length of input sequence 
 
 parser.add_argument(
     '--curriculum_learning', default='PID', choices=[None, '2zero', 'balance', 'exp', 'PID', 'IncrePID'], help='apply curriculum_learning or not')
-parser.add_argument('--PID_coefficient', default=([2, 5], [0.1, 0.2], [0.0, 0.0]), type=tuple)
+parser.add_argument('--PID_coefficient', default=([2, 10], [0.1, 1], [0.0, 0.0]), type=tuple)
 parser.add_argument('--reg_methode', default='vanilla', choices=['relu', 'log_barrier_BLS', 'vanilla'], help='regularization methode')
 parser.add_argument('--gamma', default=torch.tensor([0., 0.]), help='value of gamma', type=torch.Tensor)
 parser.add_argument('--threshold', default=torch.tensor([-0.01, -0.05]), help='value of threshold', type=torch.Tensor)
