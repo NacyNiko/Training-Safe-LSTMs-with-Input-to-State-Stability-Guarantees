ff080b1c396edb963ad5fdb0321a95d1630ace61
yinan <st171243@stud.uni-stuttgart.de>
2023-05-07 16:11:29 +0200

    local

--- Train-model-with-nonlinearProgramming/LSTM.py
+++ Train-model-with-nonlinearProgramming/LSTM.py
@@ -2,9 +2,10 @@
 # @Time : 2022/12/5 14:25 
 # @Author : Yinan 
 # @File : LSTM.py
+import copy
 
 import numpy as np
-import torch
+
 from scipy.optimize import minimize
 import pandas as pd
 import pickle
@@ -29,56 +30,69 @@ def tanh(x):
 
 def fun():
     def fun_(W):
-    # W: (146,)
         myloss = 0
 
         u = data_input
         y_true = data_output
 
-        C = np.zeros((numHiddenUnits, numInput))
-        X = np.zeros((numHiddenUnits, numInput))
+        C = np.zeros((numHiddenUnits, 1))
+        X = np.zeros((numHiddenUnits, 1))
 
         # W = [W_f, W_i, W_o, W_c, U_f, U_i, U_o, U_c, b_f, b_i, b_o, b_c, W_y, b_y]
-        W_f = W[0:5].reshape(5, 1)
-        W_i = W[5:10].reshape(5, 1)
-        W_o = W[10:15].reshape(5, 1)
-        W_c = W[15:20].reshape(5, 1)
-        U_f = W[20:45].reshape(5, 5)
-        U_i = W[45:70].reshape(5, 5)
-        U_o = W[70:95].reshape(5, 5)
-        U_c = W[95:120].reshape(5, 5)
-        b_f = W[120:125].reshape(5, 1)
-        b_i = W[125:130].reshape(5, 1)
-        b_o = W[130:135].reshape(5, 1)
-        b_c = W[135:140].reshape(5, 1)
-        W_y = W[140:145].reshape(5, 1)
-        b_y = W[145]
+        W_f = W[0:10].reshape(5, 2)
+        W_i = W[10:20].reshape(5, 2)
+        W_o = W[20:30].reshape(5, 2)
+        W_c = W[30:40].reshape(5, 2)
+        U_f = W[40:65].reshape(5, 5)
+        U_i = W[65:90].reshape(5, 5)
+        U_o = W[90:115].reshape(5, 5)
+        U_c = W[115:140].reshape(5, 5)
+        b_f = W[140:145].reshape(5, 1)
+        b_i = W[145:150].reshape(5, 1)
+        b_o = W[150:155].reshape(5, 1)
+        b_c = W[155:160].reshape(5, 1)
+        W_y = W[160:165].reshape(5, 1)
+        b_y = W[165]
 
-        for i in range(len(u)):
-            C = sigmoid(np.dot(W_f, u[i]) + np.dot(U_f, X) + b_f) * C + \
-                sigmoid(np.dot(W_i, u[i]) + np.dot(U_i, X) + b_i) * \
-                tanh(np.dot(W_c, u[i]) + np.dot(U_c, X) + b_c)
-            X = sigmoid(np.dot(W_o, u[i]) + np.dot(U_o, X) + b_o) * tanh(C)
+        for i in range(u.shape[0]):
+            C = sigmoid(np.dot(W_f, pd.DataFrame(u.iloc[i, :])) + np.dot(U_f, X) + b_f) * C + \
+                sigmoid(np.dot(W_i, pd.DataFrame(u.iloc[i, :])) + np.dot(U_i, X) + b_i) * \
+                tanh(np.dot(W_c, pd.DataFrame(u.iloc[i, :])) + np.dot(U_c, X) + b_c)
+            X = sigmoid(np.dot(W_o, pd.DataFrame(u.iloc[i, :])) + np.dot(U_o, X) + b_o) * tanh(C)
             y_pred = np.dot(W_y.T, X) + b_y
+            y_pred = y_pred * std_y + mean_y
             myloss += np.power((y_pred - y_true[i]).flatten(), 2)
+        print(f'loss:{myloss / len(u)}')
         return myloss / len(u)    # normalization
     return fun_
 
 
 def constraint():
     def constraint_(W):
-        W_f = W[0:5].reshape(5, 1)
-        W_i = W[5:10].reshape(5, 1)
-        W_o = W[10:15].reshape(5, 1)
+        W_f = W[0:10].reshape(5, 2)
+        W_i = W[10:20].reshape(5, 2)
+        W_o = W[20:30].reshape(5, 2)
 
-        U_f = W[20:45].reshape(5, 5)
-        U_i = W[45:70].reshape(5, 5)
-        U_o = W[70:95].reshape(5, 5)
-        U_c = W[95:120].reshape(5, 5)
+        U_f = W[40:65].reshape(5, 5)
+        U_i = W[65:90].reshape(5, 5)
+        U_o = W[90:115].reshape(5, 5)
+        U_c = W[115:140].reshape(5, 5)
 
-        b_f = W[120:125].reshape(5, 1)
-        b_i = W[125:130].reshape(5, 1)
-        b_o = W[130:135].reshape(5, 1)
+        b_f = W[140:145].reshape(5, 1)
+        b_i = W[145:150].reshape(5, 1)
+        b_o = W[150:155].reshape(5, 1)
+        # W_f = W[0:5].reshape(5, 1)
+        # W_i = W[5:10].reshape(5, 1)
+        # W_o = W[10:15].reshape(5, 1)
+        #
+        # U_f = W[20:45].reshape(5, 5)
+        # U_i = W[45:70].reshape(5, 5)
+        # U_o = W[70:95].reshape(5, 5)
+        # U_c = W[95:120].reshape(5, 5)
+        #
+        # b_f = W[120:125].reshape(5, 1)
+        # b_i = W[125:130].reshape(5, 1)
+        # b_o = W[130:135].reshape(5, 1)
 
         con = [1 - (1 + sigmoid(np.linalg.norm(np.hstack((W_o, U_o, b_o)), np.inf))) *
                sigmoid(np.linalg.norm(np.hstack((W_f, U_f, b_f)), np.inf)),
@@ -90,56 +104,72 @@ def constraint():
 
 def initial():
     np.random.seed(10)
-    Wf = np.random.randn(numHiddenUnits * numInput)
-    Wi = np.random.randn(numHiddenUnits * numInput)
-    Wo = np.random.randn(numHiddenUnits * numInput)
-    Wc = np.random.randn(numHiddenUnits * numInput)
-    Uf = np.random.randn(numHiddenUnits * numHiddenUnits)
-    Ui = np.random.randn(numHiddenUnits * numHiddenUnits)
-    Uc = np.random.randn(numHiddenUnits * numHiddenUnits)
-    Uo = np.random.randn(numHiddenUnits * numHiddenUnits)
-    bf = np.random.randn(numHiddenUnits * 1)
-    bi = np.random.randn(numHiddenUnits * 1)
-    bc = np.random.randn(numHiddenUnits * 1)
-    bo = np.random.randn(numHiddenUnits * 1)
-    Wy = np.random.randn(numHiddenUnits * numOutput)
-    by = np.random.randn(1)
+    Wf = np.random.randn(numHiddenUnits * (numInput + numOutput))   # 10
+    Wi = np.random.randn(numHiddenUnits * (numInput + numOutput))   # 10
+    Wo = np.random.randn(numHiddenUnits * (numInput + numOutput))   # 10
+    Wc = np.random.randn(numHiddenUnits * (numInput + numOutput))   # 10
+    Uf = np.random.randn(numHiddenUnits * numHiddenUnits)  # 25
+    Ui = np.random.randn(numHiddenUnits * numHiddenUnits)  # 25
+    Uc = np.random.randn(numHiddenUnits * numHiddenUnits)  # 25
+    Uo = np.random.randn(numHiddenUnits * numHiddenUnits)  # 25
+    bf = np.random.randn(numHiddenUnits * 1)   # 5
+    bi = np.random.randn(numHiddenUnits * 1)   # 5
+    bc = np.random.randn(numHiddenUnits * 1)   # 5
+    bo = np.random.randn(numHiddenUnits * 1)   # 5
+    Wy = np.random.randn(numHiddenUnits * numOutput)  # 5
+    by = np.random.randn(1)  # 1
 
     return np.concatenate([Wf, Wi, Wo, Wc, Uf, Ui, Uo, Uc, bf, bi, bo, bc, Wy, by])
 
 
 def load_data(train=True, noise=True):
-    if train:
-        if noise:
-            data_input = pd.read_csv("../data/train/train_input.csv", header=None)
-            data_output = pd.read_csv("../data/train/train_output.csv", header=None)
-        else:
-            data_input = pd.read_csv("../data/train/train_input_clean.csv", header=None)
-            data_output = pd.read_csv("../data/train/train_output_clean.csv", header=None)
-    else:
-        if noise:
-            data_input = pd.read_csv("../data/val/val_input.csv", header=None)
-            data_output = pd.read_csv("../data/val/val_output.csv", header=None)
-        else:
-            data_input = pd.read_csv("../data/val/val_input_clean.csv", header=None)
-            data_output = pd.read_csv("../data/val/val_output_clean.csv", header=None)
-    data_input = data_input.iloc[:, 1]
-    data_output = data_output.iloc[:, 1]
-    return data_input, data_output
+    x_mean = pd.read_csv("../data/pHdata/train/train_input.csv", header=None).iloc[:, 1].mean(axis=0)
+    x_std = pd.read_csv("../data/pHdata/train/train_input.csv", header=None).iloc[:, 1].std(axis=0)
+    y_mean = pd.read_csv("../data/pHdata/train/train_output.csv", header=None).iloc[:, 1].mean(axis=0)
+    y_std = pd.read_csv("../data/pHdata/train/train_output.csv", header=None).iloc[:, 1].std(axis=0)
+
+    train_x = pd.read_csv("../data/pHdata/train/train_input.csv", header=None).iloc[:, 1]
+    train_x = (train_x - x_mean) / x_std
+
+    train_y = pd.read_csv("../data/pHdata/train/train_output.csv", header=None).iloc[:, 1]
+    labels = copy.deepcopy(train_y)[1:]
+    labels = labels.reset_index(drop=True)
+    train_y = (train_y - y_mean) / y_std
+
+    feature = pd.concat([train_y[:-1], train_x[:-1]], axis=1)
+
+    # if train:
+    #     if noise:
+    #         data_input = pd.read_csv("../data/pHdata/train/train_input.csv", header=None)
+    #         data_output = pd.read_csv("../data/pHdata/train/train_output.csv", header=None)
+    #     else:
+    #         data_input = pd.read_csv("../data/pHdata/train/train_input_clean.csv", header=None)
+    #         data_output = pd.read_csv("../data/pHdata/train/train_output_clean.csv", header=None)
+    # else:
+    #     if noise:
+    #         data_input = pd.read_csv("../data/pHdata/val/val_input.csv", header=None)
+    #         data_output = pd.read_csv("../data/pHdata/val/val_output.csv", header=None)
+    #     else:
+    #         data_input = pd.read_csv("../data/pHdata/val/val_input_clean.csv", header=None)
+    #         data_output = pd.read_csv("../data/pHdata/val/val_output_clean.csv", header=None)
+
+    return feature, labels, y_mean, y_std
+
 
 """ hyperparameters """
 noise, train = True, True
 for if_con in [False, True]:
+    print('start optimize with constraints' if if_con else 'start optimize without constraints')
     numInput = 1
     numOutput = 1
     numHiddenUnits = 5
-    tol = 1e-10
+    tol = 10000000
 
     """ training """
     W0 = initial()
     cons = ({'type': 'ineq', 'fun': constraint()})
 
-    data_input, data_output = load_data()
+    data_input, data_output, mean_y, std_y = load_data()
 
     method_ = "SLSQP"
     time_start = time.time()
@@ -155,3 +185,4 @@ for if_con in [False, True]:
     res_file = open("models/model_{}_{}_{}_{}_{}.pkl".format("noise" if noise else "clean", method_, tol, 'w_con' if if_con else 'wo_con', now), "wb")
     pickle.dump(res, res_file)
     res_file.close()
+    print('end optimize with constraints' if if_con else 'end optimize without constraints')
--- Train-model-with-nonlinearProgramming/validation.py
+++ Train-model-with-nonlinearProgramming/validation.py
@@ -29,20 +29,20 @@ def tanh(x):
 """ validation process """
 def validate(W, data_input):
     """ LSTM """
-    W_f = W[0:5].reshape(5, 1)
-    W_i = W[5:10].reshape(5, 1)
-    W_o = W[10:15].reshape(5, 1)
-    W_c = W[15:20].reshape(5, 1)
-    U_f = W[20:45].reshape(5, 5)
-    U_i = W[45:70].reshape(5, 5)
-    U_o = W[70:95].reshape(5, 5)
-    U_c = W[95:120].reshape(5, 5)
-    b_f = W[120:125].reshape(5, 1)
-    b_i = W[125:130].reshape(5, 1)
-    b_o = W[130:135].reshape(5, 1)
-    b_c = W[135:140].reshape(5, 1)
-    W_y = W[140:145].reshape(5, 1)
-    b_y = W[145]
+    W_f = W[0:10].reshape(5, 2)
+    W_i = W[10:20].reshape(5, 2)
+    W_o = W[20:30].reshape(5, 2)
+    W_c = W[30:40].reshape(5, 2)
+    U_f = W[40:65].reshape(5, 5)
+    U_i = W[65:90].reshape(5, 5)
+    U_o = W[90:115].reshape(5, 5)
+    U_c = W[115:140].reshape(5, 5)
+    b_f = W[140:145].reshape(5, 1)
+    b_i = W[145:150].reshape(5, 1)
+    b_o = W[150:155].reshape(5, 1)
+    b_c = W[155:160].reshape(5, 1)
+    W_y = W[160:165].reshape(5, 1)
+    b_y = W[165]
 
     u = data_input
     y_val = []
@@ -58,11 +58,12 @@ def validate(W, data_input):
            sigmoid(np.linalg.norm(np.hstack((W_i, U_i, b_i)), np.inf)) * np.linalg.norm(U_c, 1)]
 
     for i in range(len(data_input)):
-        C = sigmoid(np.dot(W_f, u.iloc[i]) + np.dot(U_f, X) + b_f) * C + \
-            sigmoid(np.dot(W_i, u.iloc[i]) + np.dot(U_i, X) + b_i) * \
-            tanh(np.dot(W_c, u.iloc[i]) + np.dot(U_c, X) + b_c)
-        X = sigmoid(np.dot(W_o, u.iloc[i]) + np.dot(U_o, X) + b_o) * tanh(C)
+        C = sigmoid(np.dot(W_f, pd.DataFrame(u.iloc[i, :])) + np.dot(U_f, X) + b_f) * C + \
+            sigmoid(np.dot(W_i, pd.DataFrame(u.iloc[i, :])) + np.dot(U_i, X) + b_i) * \
+            tanh(np.dot(W_c, pd.DataFrame(u.iloc[i, :])) + np.dot(U_c, X) + b_c)
+        X = sigmoid(np.dot(W_o, pd.DataFrame(u.iloc[i, :])) + np.dot(U_o, X) + b_o) * tanh(C)
         y_pred = np.dot(W_y.T, X) + b_y
+        y_pred = y_pred * y_std + y_mean
         y_val.append(y_pred.squeeze())
     return y_val, con
 
@@ -74,51 +75,56 @@ def FIT(y_hat, y):
 def nrmse(y, y_hat):  # normalization to y
     return np.sqrt(1 / len(y)) * np.linalg.norm(y - y_hat)
 
-def main():
-    """ load trained model """
-    models = os.listdir('./models')
-    for model in models:
-        model_path = './models/' + model
-        model_file = open(model_path, "rb")
-        model = pickle.load(model_file)
-        W = model.x
 
+""" load trained model """
+models = os.listdir('./models')
+for file in models:
+    model_path = './models/' + file
+    model_file = open(model_path, "rb")
+    model = pickle.load(model_file)
+    W = model.x
 
-        f, ax = plt.subplots(2, 1)
-        i = 0
-        """ train or validation """
-        for train in [True, False]:
-            noise = True
-            if train:
-                """ load training set: 4400 samples """
-                if noise:
-                    data_input = pd.read_csv("../data/train/train_input.csv", header=None).iloc[:, 1]
-                    y_true = pd.read_csv("../data/train/train_output.csv", header=None).iloc[:, 1]
-                else:
-                    data_input = pd.read_csv("../data/train/train_input_clean.csv", header=None).iloc[:, 1]
-                    y_true = pd.read_csv("../data/train/train_output_clean.csv", header=None).iloc[:, 1]
+    f, ax = plt.subplots(2, 1)
+    i = 0
+    x_mean = pd.read_csv("../data/pHdata/train/train_input.csv", header=None).iloc[:, 1].mean(axis=0)
+    x_std = pd.read_csv("../data/pHdata/train/train_input.csv", header=None).iloc[:, 1].std(axis=0)
+    y_mean = pd.read_csv("../data/pHdata/train/train_output.csv", header=None).iloc[:, 1].mean(axis=0)
+    y_std = pd.read_csv("../data/pHdata/train/train_output.csv", header=None).iloc[:, 1].std(axis=0)
+    """ train or validation """
+    for train in [True, False]:
+        noise = True
+        if train:
+            """ load training set: 4400 samples """
+            if noise:
+                data_input = pd.read_csv("../data/pHdata/train/train_input.csv", header=None).iloc[:, 1]
+                y_true = pd.read_csv("../data/pHdata/train/train_output.csv", header=None).iloc[:, 1]
             else:
-                """ load validation set: 2250 samples """
-                if noise:
-                    data_input = pd.read_csv("../data/val/val_input.csv", header=None).iloc[:, 1]
-                    y_true = pd.read_csv("../data/val/val_output.csv", header=None).iloc[:, 1]
-                else:
-                    data_input = pd.read_csv("../data/val/val_input_clean.csv", header=None).iloc[:, 1]
-                    y_true = pd.read_csv("../data/val/val_output_clean.csv", header=None).iloc[:, 1]
+                data_input = pd.read_csv("../data/pHdata/train/train_input_clean.csv", header=None).iloc[:, 1]
+                y_true = pd.read_csv("../data/pHdata/train/train_output_clean.csv", header=None).iloc[:, 1]
+        else:
+            """ load validation set: 2250 samples """
+            if noise:
+                data_input = pd.read_csv("../data/pHdata/val/val_input.csv", header=None).iloc[:, 1]
+                y_true = pd.read_csv("../data/pHdata/val/val_output.csv", header=None).iloc[:, 1]
+            else:
+                data_input = pd.read_csv("../data/pHdata/val/val_input_clean.csv", header=None).iloc[:, 1]
+                y_true = pd.read_csv("../data/pHdata/val/val_output_clean.csv", header=None).iloc[:, 1]
+        data_input = (data_input - x_mean) / x_std
 
-            """ calculate y_val """
-            y_val, cons = validate(W, data_input)
-
-            """ plot """
-            nrmse_score = nrmse(y_val, y_true)
-            f.suptitle('c1:{} c2:{}'.format(cons[0], cons[1]))
-            ax[i].plot(y_val, color='m', label='pred', alpha=0.8)
-            ax[i].plot(y_true, color='c', label='real', linestyle='--', alpha=0.5)
-            ax[i].tick_params(labelsize=5)
-            ax[i].legend(loc='best')
-            ax[i].set_title('NRMSE on {} set: {:.3f}'.format('train' if train else 'val', nrmse_score), fontsize=8)
-            i += 1
-        plt.savefig('./result/python/fig/{}.jpg'.format(np.random.randint(100)), bbox_inches='tight', dpi=500)
+        """ calculate y_val """
+        y_val, cons = validate(W, data_input)
+        y_val = pd.Series(y_val)
+        y_val = y_val * y_std + y_mean
+        """ plot """
+        nrmse_score = nrmse(y_val, y_true)
+        f.suptitle('c1:{} c2:{}'.format(cons[0], cons[1]))
+        ax[i].plot(y_val, color='m', label='pred', alpha=0.8)
+        ax[i].plot(y_true, color='c', label='real', linestyle='--', alpha=0.5)
+        ax[i].tick_params(labelsize=5)
+        ax[i].legend(loc='best')
+        ax[i].set_title('NRMSE on {} set: {:.3f}'.format('train' if train else 'val', nrmse_score), fontsize=8)
+        i += 1
+    plt.savefig('./result/python/fig/{}.jpg'.format(file[:-4]), bbox_inches='tight', dpi=500)
             # plt.figure()
             # plt.title('constraints: c1:{}, c2:{}'.format(cons[0], cons[1]))
             # plt.plot([*range(len(data_input) - 100)], y_val[100:], color='b', label='Prediction')
@@ -129,7 +135,5 @@ def main():
             # plt.legend()
             # plt.title('pH on {} set with FIT: {:.3f}%'.format('train' if train else "validation", fit_))
             # plt.show()
-if __name__ == '__main__':
-    main()
 
 
