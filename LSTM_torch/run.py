cd68da16c6c74df467f1b97f92ff62d060b0fb3d
yinan <st171243@stud.uni-stuttgart.de>
2023-05-12 19:41:29 +0200

    local

--- LSTM_torch/run.py
+++ LSTM_torch/run.py
@@ -13,28 +13,28 @@ import validation
 
 parser = argparse.ArgumentParser(description='Input state stable LSTM')
 parser.add_argument('--device', default='cuda:0', choices=['cpu', 'cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'])
-parser.add_argument('--dataset', default='pHdata', choices=['pHdata', 'robot_forward', 'coupled electric drives'], help='LSTM dataset')
+parser.add_argument('--dataset', default='robot_forward', choices=['pHdata', 'robot_forward', 'coupled electric drives'], help='LSTM dataset')
 parser.add_argument('--hidden_size', default=5, help='hidden size of LSTM', type=int)
 
-parser.add_argument('--input_size', default=1, help='input size of LSTM', type=int)
-parser.add_argument('--output_size', default=1, help='output size of output layer', type=int)
-parser.add_argument('--layers', default=1, help='number of layers of LSTM', type=int)
-parser.add_argument('--batch_size', default=64, help='train batch size', type=int)
-parser.add_argument('--epochs', default=100, help='maximum train epochs', type=int)
+parser.add_argument('--input_size', default=6, help='input size of LSTM', type=int)
+parser.add_argument('--output_size', default=6, help='output size of output layer', type=int)
+parser.add_argument('--layers', default=5, help='number of layers of LSTM', type=int)
+parser.add_argument('--batch_size', default=128, help='train batch size', type=int)
+parser.add_argument('--epochs', default=1000, help='maximum train epochs', type=int)
 parser.add_argument('--tolerance', default=-1e-3, help='minimum tolerance of loss', type=float)
 parser.add_argument('--tol_stop', default=-0.1, help='minimum tolerance between 2 epochs', type=float)
-parser.add_argument('--len_sequence', default=10, help='length of input sequence to LSTM', type=int)
+parser.add_argument('--len_sequence', default=40, help='length of input sequence to LSTM', type=int)
 
 parser.add_argument(
     '--curriculum_learning', default=None, choices=[None, '2zero', 'balance', 'exp', 'PID', 'IncrePID'], help='apply curriculum_learning or not')
 parser.add_argument('--dynamic_K', default=False, type=bool)
 parser.add_argument('--PID_coefficient', default=([3, 1], [0.2, 1], [0.5, 0.5]), type=tuple)
-parser.add_argument('--reg_methode', default='log_barrier_BLS', choices=['relu', 'log_barrier_BLS', 'vanilla'], help='regularization methode')
-parser.add_argument('--gamma', default=torch.tensor([1., 1.]), help='value of gamma', type=torch.Tensor)
+parser.add_argument('--reg_methode', default='relu', choices=['relu', 'log_barrier_BLS', 'vanilla'], help='regularization methode')
+parser.add_argument('--gamma', default=torch.tensor([0., 0.]), help='value of gamma', type=torch.Tensor)
 parser.add_argument('--threshold', default=torch.tensor([0.0, 0.0]), help='value of threshold', type=torch.Tensor)
 
 if __name__ == '__main__':
-    grid_Search = False
+    grid_Search = True
     if grid_Search:
         # threshold_values = [x for x in range(0, 10)]
         # gamma_values = [x for x in range(0, 10)]
@@ -46,8 +46,8 @@ if __name__ == '__main__':
         #         args.threshold = torch.tensor([threshold, threshold])
         #         args.gamma = torch.tensor([gamma, gamma])
 
-        hidden_values = [160, 200]
-        len_sequence_values = [30, 40]
+        hidden_values = [100, 150, 250, 300]
+        len_sequence_values = [40]
         for hs in hidden_values:
             for ls in len_sequence_values:
                 print('hidden size:{}, seq length:{}'.format(hs, ls))
