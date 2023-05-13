1e327652146ed9a4c22d89e5594c9b849d961fde
yinan <st171243@stud.uni-stuttgart.de>
2023-05-13 21:09:45 +0200

    local5.13

--- LSTM_torch/run.py
+++ LSTM_torch/run.py
@@ -18,7 +18,7 @@ parser.add_argument('--hidden_size', default=150, help='hidden size of LSTM', ty
 
 parser.add_argument('--input_size', default=6, help='input size of LSTM', type=int)
 parser.add_argument('--output_size', default=6, help='output size of output layer', type=int)
-parser.add_argument('--layers', default=5, help='number of layers of LSTM', type=int)
+parser.add_argument('--layers', default=2, help='number of layers of LSTM', type=int)
 parser.add_argument('--batch_size', default=128, help='train batch size', type=int)
 parser.add_argument('--epochs', default=100, help='maximum train epochs', type=int)
 parser.add_argument('--tolerance', default=-1e-3, help='minimum tolerance of loss', type=float)
@@ -31,7 +31,7 @@ parser.add_argument('--dynamic_K', default=False, type=bool)
 parser.add_argument('--PID_coefficient', default=([3, 1], [0.2, 1], [0.5, 0.5]), type=tuple)
 parser.add_argument('--reg_methode', default='relu', choices=['relu', 'log_barrier_BLS', 'vanilla'], help='regularization methode')
 parser.add_argument('--gamma', default=torch.tensor([0., 0.]), help='value of gamma', type=torch.Tensor)
-parser.add_argument('--threshold', default=torch.tensor([0.0, 0.0]), help='value of threshold', type=torch.Tensor)
+parser.add_argument('--threshold', default=torch.tensor([0.1, 0.1]), help='value of threshold', type=torch.Tensor)
 
 if __name__ == '__main__':
     grid_Search = True
