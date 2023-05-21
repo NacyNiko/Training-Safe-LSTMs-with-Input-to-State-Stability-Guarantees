5cbb8bae13ea85a9fc6b9d450dc7b613524e2c4b
yinan <st171243@stud.uni-stuttgart.de>
2023-05-21 20:10:51 +0200

    train PID both

--- LSTM_torch/run.py
+++ LSTM_torch/run.py
@@ -67,9 +67,9 @@ if __name__ == '__main__':
     #     print(f'total times:{-start+end}')
     #     validation.main(parser.parse_args(), if_recoder=False, piecewise=True)
 
-    for cl, rm, dy in [('pid', 'vanilla', False)]:
+    for cl, rm, dy in [('PID', 'vanilla', False)]:
         # (None, 'relu'), ('2part', 'vanilla'), ('2zero', 'vanilla'), ('balance', 'relu'), ('exp', 'vanilla')
-        for dataset in ['robot_forward']:
+        for dataset in ['pHdata', 'robot_forward']:
             if dataset == 'pHdata':
                 hs = 5
                 l = 1
