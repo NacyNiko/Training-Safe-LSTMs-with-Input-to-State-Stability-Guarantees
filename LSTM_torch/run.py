cf2dc9e0db2559dec82c3435f99ed3c57182dab8
yinan <st171243@stud.uni-stuttgart.de>
2023-05-22 21:48:17 +0200

    train PID

--- LSTM_torch/run.py
+++ LSTM_torch/run.py
@@ -32,7 +32,7 @@ parser.add_argument('--dynamic_K', default=False, type=bool)
 parser.add_argument('--PID_coefficient', default=([0.5, 1.0], [0.1, 0.1], [0.0, 0.0]), type=tuple)
 parser.add_argument('--reg_methode', default='vanilla', choices=['relu', 'log_barrier_BLS', 'vanilla'], help='regularization methode')
 parser.add_argument('--gamma', default=torch.tensor([0.1, 500]), help='value of gamma', type=torch.Tensor)
-parser.add_argument('--threshold', default=torch.tensor([-0.02, -0.05]), help='value of threshold', type=torch.Tensor)
+parser.add_argument('--threshold', default=torch.tensor([-0.05, -0.1]), help='value of threshold', type=torch.Tensor)
 
 
 if __name__ == '__main__':
