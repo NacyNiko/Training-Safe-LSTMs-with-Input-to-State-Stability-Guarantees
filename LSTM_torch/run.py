b83324b366229bddf71ffd5426f0d2e3cf333736
yinan <st171243@stud.uni-stuttgart.de>
2023-05-22 12:00:22 +0200

    train pid ROB

--- LSTM_torch/run.py
+++ LSTM_torch/run.py
@@ -29,7 +29,7 @@ parser.add_argument('--len_sequence', default=40, help='length of input sequence
 parser.add_argument(
     '--curriculum_learning', default='exp', choices=[None, '2part', '2zero', 'balance', 'exp', 'PID', 'IncrePID'], help='apply curriculum_learning or not')
 parser.add_argument('--dynamic_K', default=False, type=bool)
-parser.add_argument('--PID_coefficient', default=([2, 5], [0.1, 0.1], [0.0, 0.0]), type=tuple)
+parser.add_argument('--PID_coefficient', default=([0.5, 1.0], [0.1, 0.1], [0.0, 0.0]), type=tuple)
 parser.add_argument('--reg_methode', default='vanilla', choices=['relu', 'log_barrier_BLS', 'vanilla'], help='regularization methode')
 parser.add_argument('--gamma', default=torch.tensor([0.0, 0.0]), help='value of gamma', type=torch.Tensor)
 parser.add_argument('--threshold', default=torch.tensor([-0.05, -0.1]), help='value of threshold', type=torch.Tensor)
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     for cl, rm, dy in [('PID', 'vanilla', False)]:
         # (None, 'relu'), ('2part', 'vanilla'), ('2zero', 'vanilla'), ('balance', 'relu'), ('exp', 'vanilla')
-        for dataset in ['robot_forward']:
+        for dataset in ['pHdata']:
             if dataset == 'pHdata':
                 hs = 5
                 l = 1
